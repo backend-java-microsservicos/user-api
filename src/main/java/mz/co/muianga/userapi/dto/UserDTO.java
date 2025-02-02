898789e6c15ea08284f1ce18d2471d0fe433d958
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-02 08:30:41 +0200

    chore: update user dto

--- src/main/java/mz/co/muianga/userapi/dto/UserDTO.java
+++ src/main/java/mz/co/muianga/userapi/dto/UserDTO.java
@@ -3,6 +3,7 @@ package mz.co.muianga.userapi.dto;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
+import mz.co.muianga.userapi.model.User;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,4 +20,14 @@ public class UserDTO {
     private String telefone;
     private Date dataCadastro;
 
+    public static UserDTO convert(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome(user.getNome());
+        userDTO.setEndereco(user.getEndereco());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setTelefone(user.getTelefone());
+        userDTO.setDataCadastro(user.getDataCadastro());
+        return userDTO;
+    }
 }
