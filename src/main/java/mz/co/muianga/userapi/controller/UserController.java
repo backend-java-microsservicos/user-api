ba20527cb31ae2de8b07c931fee6f3fc755a8344
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-02 08:41:36 +0200

    chore: refactor user controller
    - replace static list for user service

--- src/main/java/mz/co/muianga/userapi/controller/UserController.java
+++ src/main/java/mz/co/muianga/userapi/controller/UserController.java
@@ -1,80 +1,44 @@
 package mz.co.muianga.userapi.controller;
 
-import jakarta.annotation.PostConstruct;
 import mz.co.muianga.userapi.dto.UserDTO;
+import mz.co.muianga.userapi.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    public static List<UserDTO> usuarios = new ArrayList<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public List<UserDTO> getUsers() {
-        return usuarios;
+        return userService.getAll();
     }
 
     @GetMapping("/{cpf}")
     public UserDTO getUsersFiltro(@PathVariable("cpf") String cpf) {
-        for (UserDTO userFilter : usuarios) {
-            if (userFilter.getCpf().equals(cpf)) {
-                return userFilter;
-            }
-        }
-        return null;
+        return userService.findByCpf(cpf);
     }
 
-    @PostMapping("/newUser")
-    UserDTO inserir(@RequestBody UserDTO userDTO) {
-        userDTO.setDataCadastro(new Date());
-        usuarios.add(userDTO);
-        return userDTO;
+    @PostMapping
+    public UserDTO inserir(@RequestBody UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("/{cpf}")
-    public boolean remover(@PathVariable String cpf) {
-        for(UserDTO user : usuarios) {
-            if(user.getCpf().equals(cpf)) {
-                usuarios.remove(user);
-                return true;
-            }
-        }
-        return false;
+    @DeleteMapping("/{id}")
+    public UserDTO remover(@PathVariable("id") long id) {
+        return userService.delete(id);
     }
 
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setNome("Eduardo");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua a");
-        userDTO.setEmail("eduardo@email.com");
-        userDTO.setTelefone("1234-3454");
-        userDTO.setDataCadastro(new Date());
-
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setNome("Luiz");
-        userDTO2.setCpf("456");
-        userDTO2.setEndereco("Rua b");
-
-        userDTO2.setEmail("luiz@email.com");
-        userDTO2.setTelefone("1234-3454");
-        userDTO2.setDataCadastro(new Date());
-
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setNome("Bruna");
-        userDTO3.setCpf("678");
-        userDTO3.setEndereco("Rua c");
-        userDTO3.setEmail("bruna@email.com");
-        userDTO3.setTelefone("1234-3454");
-        userDTO3.setDataCadastro(new Date());
-        usuarios.add(userDTO);
-        usuarios.add(userDTO2);
-        usuarios.add(userDTO3);
+    @GetMapping("/search")
+    public List<UserDTO> queryByName(@RequestParam(name = "nome", required = true) String nome) {
+        return userService.queryByName(nome);
     }
+
 }
